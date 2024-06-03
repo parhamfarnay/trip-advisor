04bbaf2685c723449f2c17d1d0ed5e8d3830db39
parhamfarnay <prhmfrny@gmail.com>
2024-06-03 13:06:51 +0330

    updated images remote patterns

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Image
+        src={
+          "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2b/26/f7/54/caption.jpg"
+        }
+        alt="soxom mehdiya"
+        width={100}
+        height={20}
+      />
+    </main>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dynamic-media-cdn.tripadvisor.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
